09210a119e2e594bd1fbd90174ca881dccc66135
HANTING JIN <jinhanting@gmail.com>
2025-11-15 19:51:13 +1100

    1. install tailwind

--- reservations-app/vite.config.ts
+++ reservations-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    // 类型上与不同版本的 Vite Plugin 不兼容，这里直接断言为 any 以避免 TS 报错
+    tailwindcss() as any,
   ],
 })
